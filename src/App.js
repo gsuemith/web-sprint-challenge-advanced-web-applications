696ae832abfff1f0c8961e1d762fda2538fe765a
Garrick Suemith <garrick.suemith@gmail.com>
2021-03-26 03:45:50 -0400

    private route redirecting

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React, { useState } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import Login from "./components/Login";
+import BubblePage from "./components/BubblePage"
+
+import PrivateRoute from "./components/PrivateRoute";
 import "./styles.scss";
 
 function App() {
   return (
     <Router>
       <div className="App">
+        <PrivateRoute path="/bubbles" component={BubblePage}/>
         <Route exact path="/" component={Login} />
       </div>
     </Router>
